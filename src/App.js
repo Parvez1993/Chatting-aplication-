e67b6c066c8784d70250528aaafeecd1cad35640
PARVEZ1993 <mohammedparvez1923@gmail.com>
2021-11-19 18:35:45 +0600

    login

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/login" element={<Signin />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
---
+++ src/components/Signup.js
@@ -0,0 +1,197 @@
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "@firebase/auth";
+import React, { Component } from "react";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Icon,
+  Message,
+  Segment,
+} from "semantic-ui-react";
+import { getDatabase, ref, set } from "firebase/database";
+import { auth } from "./config";
+import { Link } from "react-router-dom";
+
+export default class Signup extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    errMesg: "",
+    success: "",
+    loading: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  formValidation = ({ username, email, password, confirmPassword }) => {
+    if (!username || !email || !password || !confirmPassword) {
+      this.setState({ errMesg: "Please fill all the forms" });
+    } else if (password.length < 8) {
+      this.setState({ errMesg: "Password too weak" });
+    } else if (password !== confirmPassword) {
+      this.setState({ errMesg: "Passwrod do not match" });
+    } else {
+      return true;
+    }
+    this.setState({ loading: false });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    const auth = getAuth();
+    if (this.formValidation(this.state)) {
+      createUserWithEmailAndPassword(
+        auth,
+        this.state.email,
+        this.state.password
+      )
+        .then((userCredential) => {
+          updateProfile(auth.currentUser, {
+            displayName: this.state.username,
+            // photoURL: "https://example.com/jane-q-user/profile.jpg",
+          })
+            .then(() => {
+              this.writeUserData(userCredential);
+            })
+            .then(() => {
+              this.setState({ username: "" });
+              this.setState({ email: "" });
+              this.setState({ password: "" });
+              this.setState({ confirmPassword: "" });
+              this.setState({ errMesg: "" });
+              this.setState({ success: "Account created successfully" });
+              this.setState({ loading: false });
+            })
+            .catch((error) => {
+              this.setState({ loading: false });
+              const errorCode = error.code;
+              if (errorCode) {
+                this.setState({ errorMesg: "username already in use" });
+              }
+            });
+        })
+
+        .catch((error) => {
+          this.setState({ loading: false });
+          const errorCode = error.code;
+          if (errorCode.includes("email")) {
+            this.setState({ errorMesg: "email already in use" });
+          }
+        });
+    }
+  };
+
+  writeUserData = (user) => {
+    const db = getDatabase();
+    set(ref(db, "users/" + user.user.uid), {
+      username: this.state.username,
+    });
+  };
+
+  render() {
+    const {
+      username,
+      email,
+      password,
+      confirmPassword,
+      errMesg,
+      success,
+      loading,
+    } = this.state;
+
+    return (
+      <>
+        <Grid verticalAlign="center">
+          <Grid.Column style={{ maxWidth: 400 }}>
+            <Header as="h2" icon textAlign="center">
+              <Icon name="sign-in" />
+              <Header.Content>Register</Header.Content>
+            </Header>
+            {errMesg ? (
+              <Message warning floating>
+                {errMesg}
+              </Message>
+            ) : (
+              ""
+            )}
+            {success ? (
+              <Message success floating>
+                {success}
+              </Message>
+            ) : (
+              ""
+            )}
+            <Segment raised>
+              <Form style={{ textAlign: "left" }}>
+                <Form.Field>
+                  <label>Username</label>
+                  <input
+                    name="username"
+                    type="text"
+                    onChange={this.handleChange}
+                    placeholder="username"
+                    value={username}
+                  />
+                </Form.Field>
+                <Form.Field
+                  className={errMesg.includes("email") ? "error" : ""}
+                >
+                  <label>Email</label>
+                  <input
+                    placeholder="email"
+                    name="email"
+                    type="email"
+                    onChange={this.handleChange}
+                    value={email}
+                  />
+                </Form.Field>
+                <Form.Field
+                  className={errMesg.includes("Password") ? "error" : ""}
+                >
+                  <label>Password</label>
+                  <input
+                    name="password"
+                    type="password"
+                    onChange={this.handleChange}
+                    placeholder="password"
+                    value={password}
+                  />
+                </Form.Field>
+                <Form.Field>
+                  <label>Confirm Password</label>
+                  <input
+                    placeholder="confirm password"
+                    name="confirmPassword"
+                    type="password"
+                    onChange={this.handleChange}
+                    value={confirmPassword}
+                  />
+                </Form.Field>
+                <Button
+                  className={loading ? "loading primary" : ""}
+                  type="submit"
+                  onClick={this.handleSubmit}
+                >
+                  Submit
+                </Button>
+              </Form>
+            </Segment>
+            <Segment>
+              Already have an account <Link to="/login">Click here</Link>
+            </Segment>
+          </Grid.Column>
+        </Grid>
+      </>
+    );
+  }
+}
